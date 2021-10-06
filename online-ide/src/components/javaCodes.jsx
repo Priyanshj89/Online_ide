7198309991c23fe207f82eb6793c15baff8f4197
Priyansh.j89 <55475866+Priyanshj89@users.noreply.github.com>
2021-10-06 17:48:32 +0530

    Added java pattern program

--- online-ide/src/components/javaCodes.jsx
+++ online-ide/src/components/javaCodes.jsx
@@ -3,3 +3,26 @@ export const hello = `public class Hello{
         System.out.println("Hello World");
     }
 }`;
+
+export const pattern = `import java.io.*;
+public class GeeksForGeeks
+{
+	public static void printStars(int n)
+	{
+		int i, j;
+		for(i=0; i<n; i++)
+		{
+			for(j=0; j<=i; j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+}
+	public static void main(String args[])
+	{
+		int n = 5;
+		printStars(n);
+	}
+}
+`;
